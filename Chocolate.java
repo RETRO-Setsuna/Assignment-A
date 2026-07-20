9f7ba8c78f58f8d30cb719ec1c6d6557e900f264
Percy2234 <hyun9henry@gmail.com>
2026-07-20 12:13:36 +1000

    Refactor Chocolate.java to include enums and payment classes
    Added enums for Size, Delivery, Sweetness, Types and classes for payment methods.

--- Chocolate.java
+++ Chocolate.java
@@ -1,5 +1,45 @@
-public class Chocolate {
-    public static void main(String[] args) {
-        System.out.println("Choco Test");
+enum Size {
+    S, M, L, XL;
+}
+
+enum Delivery {
+    PICK_UP, DELIVERY;
+}
+
+enum FILLINGS {
+}
+
+enum Sweetness {
+    ZERO(0), TWENTY_FIVE(25), FIFTY(50), SEVENTY_FIVE(75), HUNDRED(100);
+
+    private int percentage;
+
+    Sweetness(int percentage) {
+        this.percentage = percentage;
     }
+
+    public int getPercentage() {
+        return percentage;
+    }
+}
+
+enum Toppings {
+}
+
+enum Types {
+    WHITE_CHOCOLATE, DARK_CHOCOLATE, MILK_CHOCOLATE, COOKIE_AND_CREAM;
+}
+
+class payment {
+
+}
+
+class cashPayment extends payment {
+
+}
+
+class cardPayment extends payment {
+}
+
+interface choose {
 }
