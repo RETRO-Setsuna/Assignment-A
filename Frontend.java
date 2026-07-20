885fb928cca476c384b384dd2065210adaccb0db
RETRO-Setsuna <rennywu333@gmail.com>
2026-07-20 12:51:26 +1000

    Add main menu with options for users
    Implement main menu for HD Chocolate Factory application.

---
+++ Frontend.java
@@ -0,0 +1,24 @@
+class Menu {
+    boolean running = true;
+
+    while (running = true) {
+        System.out.println("Welcome to HD Chocolate Factory!");
+            System.out.println("Please select a option from below");
+            System.out.println("1. Membership Sign in");
+            System.out.println("2. Search for a Chocolate");
+            System.out.println("3. Build a Chocolate");
+            System.out.println("4. My cart");
+            System.out.println("5. Reorder");
+            System.out.println("6. Exit");
+    }
+    userSelect = ln.nextint;
+
+    if (userSelect = 1) {
+
+    } else if (userSelect = 2) {
+
+    } else if (userSelect = 3) {
+        
+    }
+    
+}
