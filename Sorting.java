f6fe0ff8475f446e850dad352556d0798bc91424
RETRO_Setsuna <rennywu333@gmail.com>
2026-07-23 22:46:43 +1000

    fixing???

--- Sorting.java
+++ Sorting.java
@@ -1,3 +1,15 @@
-public class Sorting {
-    
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+
+class sortingABC {
+    static final Comparator<Chocolate> comparator = Comparator.comparing(Chocolate::productName).thenComparing(Chocolate::productID);
+
+    Collections.sort(ArrayList, Chocolate.comparator);
+
+    for (Chocolate c : ArrayList) {
+        System.out.println(c);
+    }
+
+
 }
