a5e9acfa6716e9f3dfd5b19278405b10e34262f7
RETRO-Setsuna <rennywu333@gmail.com>
2026-07-27 18:05:47 +1000

    testing something

--- Users.java
+++ Users.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class Users {
     private String name;
     private String phoneNumber;
@@ -18,4 +22,48 @@ class Users {
     public String toString() {
         return "Name: " + name + "    Phone Number: " + phoneNumber;
     }
+}
+
+
+interface Requirment {
+    boolean satisfaction(String values);
+}
+
+class MiniReq implements Requirment {
+    int miniLen;
+
+    MiniReq(int miniLen) {
+        this.miniLen = miniLen;
+    }
+
+    @Override
+    public boolean satisfaction(String values) {
+        if (values.length() >= miniLen) {
+            return true;
+    }
+}
+
+class logInCredentials {
+    private String userTypes;
+
+    logInCredentials(String userTypes) {
+        this.userTypes = userTypes;
+    }
+
+    public String getUserTypes() {
+        return this.userTypes;
+    }
+
+    public void setUserTypes(String userTypes) {
+        this.userTypes = userTypes;
+    }
+
+    public String toString() {
+        return userTypes;
+    }
+
+}   
+
+class Passwords extends logInCredentials {
+    
 }
